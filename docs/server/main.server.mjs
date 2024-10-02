023faac7c3ee15d2a93c6497fdbc63ef9a0beb04
Hrushikesh <hrushikeshsurkar@gmial.com>
2024-10-02 19:08:12 +0530

    github pages added

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ARUETZOO.mjs";import"./chunk-R2ON4LOY.mjs";import"./chunk-DN33APEY.mjs";import"./chunk-XJAVNBJ7.mjs";import"./chunk-VBNS6X6X.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
